2e4504938946aa310507157d09c3adef3406e108
Bryan Perez <bryanprz00@gmail.com>
2025-07-23 05:43:53 -0500

    Update tests to use memoization

--- spec/dino_spec.rb
+++ spec/dino_spec.rb
@@ -2,36 +2,34 @@ require 'rspec'
 require_relative '../lib/dino_management/dino'
 
 describe Dino do
+  let(:carnivore_dino) { Dino.new(name: 'Rex', age: 100, category: 'carnivore', diet: 'meat', period: 'Cretaceous') }
+  let(:herbivore_dino) { Dino.new(name: 'Triceratops', age: 80, category: 'herbivore', diet: 'plants', period: 'Cretaceous') }
+
   it 'initializes with correct attributes' do
-    dino = Dino.new(name: 'Rex', age: 100, category: 'carnivore', diet: 'meat', period: 'Cretaceous')
-    expect(dino.name).to eq('Rex')
-    expect(dino.age).to eq(100)
-    expect(dino.category).to eq('carnivore')
-    expect(dino.diet).to eq('meat')
-    expect(dino.period).to eq('Cretaceous')
+    expect(carnivore_dino.name).to eq('Rex')
+    expect(carnivore_dino.age).to eq(100)
+    expect(carnivore_dino.category).to eq('carnivore')
+    expect(carnivore_dino.diet).to eq('meat')
+    expect(carnivore_dino.period).to eq('Cretaceous')
   end
 
   describe '#carnivore?' do
     it 'returns true if the dino is a carnivore' do
-      dino = Dino.new(name: 'Rex', age: 100, category: 'carnivore', diet: 'meat', period: 'Cretaceous')
-      expect(dino.carnivore?).to be true
+      expect(carnivore_dino.carnivore?).to be true
     end
 
     it 'returns false if the dino is not a carnivore' do
-      dino = Dino.new(name: 'Rex', age: 100, category: 'herbivore', diet: 'plants', period: 'Cretaceous')
-      expect(dino.carnivore?).to be false
+      expect(herbivore_dino.carnivore?).to be false
     end
   end
 
   describe '#herbivore?' do
     it 'returns true if the dino is a herbivore' do
-      dino = Dino.new(name: 'Rex', age: 100, category: 'herbivore', diet: 'plants', period: 'Cretaceous')
-      expect(dino.herbivore?).to be true
+      expect(herbivore_dino.herbivore?).to be true
     end
 
     it 'returns false if the dino is not a herbivore' do
-      dino = Dino.new(name: 'Rex', age: 100, category: 'carnivore', diet: 'meat', period: 'Cretaceous')
-      expect(dino.herbivore?).to be false
+      expect(carnivore_dino.herbivore?).to be false
     end
   end
 end
